02b07547bd3cdd8fd53b852fcdb95c7d93f90aa9
Peter Schueller <peter@peterschueller.com>
2021-08-19 18:35:20 +0200

    added license

--- orchestrator_client/orchestrator_client.py
+++ orchestrator_client/orchestrator_client.py
@@ -1,5 +1,21 @@
-# generic parallel orchestrator client
+#!/usr/bin/env python3
+# ===================================================================================
+# Copyright (C) 2021 Fraunhofer Gesellschaft, Peter Schueller. All rights reserved.
+# ===================================================================================
+# This Acumos software file is distributed by Fraunhofer Gesellschaft
+# under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
 #
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# This file is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ===============LICENSE_END========================================================
+
+# generic parallel orchestrator client
 # arguments:
 # --ip/-i <ip> --port/-p <port> --endpoint/-e <ip>:<port>
 # functionality:
