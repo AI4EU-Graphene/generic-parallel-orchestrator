1976ebcdc65f02269b44292a6b47095b1c7505aa
Peter Schueller <peter@peterschueller.com>
2021-08-20 15:32:04 +0200

    script for copying to kubernetes client

---
+++ scripts/copy_to_kubernetes_client_repo.sh
@@ -0,0 +1,6 @@
+REPO=../kubernetes-client/deploy/private/
+cp orchestrator_client/orchestrator_client.py $REPO/orchestrator_client/
+cp orchestrator_client/orchestrator_pb2_grpc.py $REPO/orchestrator_client/
+cp orchestrator_client/orchestrator_pb2.py $REPO/orchestrator_client/
+cp orchestrator_client/requirements.txt $REPO/orchestrator_client/
+cp protobuf/orchestrator.proto $REPO/protobuf/
